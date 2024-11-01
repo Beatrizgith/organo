41f89c436e25001c5ed51a397206f6c7cad01d00
Beatriz <beatriz.edrd.bezerra@gmail.com>
2024-11-01 13:03:53 -0300

    Meu primeiro commit no branch principal

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,16 @@
+import './Banner.css'
+
+
+export const Banner = () =>{
+
+    //JSX
+    return(
+        <header className ="banner">
+            
+        <img src="/imagens/Banner.png" alt="Imagem do banner principal do projeto"/>
+        </header>
+        
+    )
+
+}
+
